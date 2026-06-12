7cda0d69d33717bdef3fff3373d86aaa651c8d31
窩窩 <30565934+kingxzk@users.noreply.github.com>
2026-06-12 14:26:09 +0800

    美石科技配置文件

--- src/common.rs
+++ src/common.rs
@@ -990,7 +990,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://rd.wowo.run:21114".to_owned()
+    "http://rd.milestones.ltd:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
